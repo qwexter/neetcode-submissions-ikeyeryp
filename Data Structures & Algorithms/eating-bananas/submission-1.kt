3f3876a6b0d9aa8a9d073d91335648582b6cbbed
Aleksandr Bubnov <69006636+qwexter@users.noreply.github.com>
2026-03-30 20:15:20 +0400

    Add: eating-bananas - submission-1

---
+++ Data Structures & Algorithms/eating-bananas/submission-1.kt
@@ -0,0 +1,16 @@
+class Solution {
+    fun minEatingSpeed(piles: IntArray, h: Int): Int {
+        var r = piles.max()
+        var l = 1
+        while (l < r) {
+            val m = l + (r - l) / 2
+            val sum = piles.sumOf { Math.ceil(it.toDouble() / m) }
+            if (sum <= h) {
+                r = m
+            } else {
+                l = m + 1
+            }
+        }
+        return l
+    } 
+}
